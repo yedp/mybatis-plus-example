7a3d057a2e0be8847b32eafe69ef9bba1f2946ea
叶道平 <daoping.ye@56qq.com>
2019-01-16 17:33:22 +0800

    log4j2日志添加

--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -1,11 +1,59 @@
 package com.example.entity;
 
-import lombok.Data;
 
-@Data
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+
 public class User {
     private Long id;
     private String name;
     private Integer age;
     private String email;
+    private Integer isDel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getIsDel() {
+        return isDel;
+    }
+
+    public void setIsDel(Integer isDel) {
+        this.isDel = isDel;
+    }
+
+    @Override
+    public String toString() {
+        return JSON.toJSONString(this);
+    }
 }
--- src/main/resources/db/data-h2.sql
+++ src/main/resources/db/data-h2.sql
@@ -5,4 +5,5 @@ INSERT INTO user (id, name, age, email) VALUES
 (2, 'Jack', 20, 'test2@baomidou.com'),
 (3, 'Tom', 28, 'test3@baomidou.com'),
 (4, 'Sandy', 21, 'test4@baomidou.com'),
-(5, 'Billie', 24, 'test5@baomidou.com');
+(5, 'Billie', 24, 'test5@baomidou.com'),
+(6, 'ydp', 24, 'test6@baomidou.com');
